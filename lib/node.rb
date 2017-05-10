677ad858838b1f572b8e18a39789cf2886dc265d
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-05-10 17:02:36 -0600

    node file extension

--- lib/node.rb
+++ lib/node.rb
@@ -1,14 +1,29 @@
 class Node
 
   def initialize(data)
+    @head
     @data = data
+    @next_node = nil
+  end
+  def append(arg)
+    @head = Node.new(arg)
+    @next_node = @head.object_id
+    arg
+  end
+
+  def head
+    @head
   end
 
   def data
     @data
   end
 
+  def to_string
+    x = @data
+  end
+
   def next_node
-    nil
+    @head
   end
 end
